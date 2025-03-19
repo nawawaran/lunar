6f7642628df5d781bceb61879133caec089a92a1
samimhamza <samim.hamza7@gmail.com>
2025-03-19 13:20:23 +0330

    tenant_id removed from media

--- packages/core/database/migrations/2021_08_10_101547_create_media_table.php
+++ packages/core/database/migrations/2021_08_10_101547_create_media_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Lunar\Base\Migration;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
-        if (! Schema::hasTable('media')) {
+        if (!Schema::hasTable('media')) {
             Schema::create('media', function (Blueprint $table) {
                 $table->bigIncrements('id');
 
                 $table->morphs('model');
-                $table->uuid('uuid')->nullable(); // ->unique()
+                $table->uuid('uuid')->nullable()->unique();
                 $table->string('collection_name');
                 $table->string('name');
                 $table->string('file_name');
@@ -28,8 +27,6 @@ return new class extends Migration
                 $table->unsignedInteger('order_column')->nullable();
 
                 $table->nullableTimestamps();
-                $table->foreignUuid('tenant_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-                $table->unique(['uuid', 'tenant_id']);
             });
         }
     }
